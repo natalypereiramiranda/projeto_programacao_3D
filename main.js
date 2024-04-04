f8dfda7260832f2337d7a4f7ee4606f296e1f856
natalypereiramiranda <110977847+natalypereiramiranda@users.noreply.github.com>
2024-04-04 11:15:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-04-05T00:00:00");
+
+contadores[0].textContent = calculeTempo (tempoObjetivo1);
+
+function calculeTempo (tempoObjetivo1){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segudos";
+}
 
 for (let i=0; i < botoes.length; i++){
     botoes[i].onclick=function(){
         
         for(let j=0; j<botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
